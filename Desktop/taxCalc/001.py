e591442a1a4650547a4e0529f59f5cd8e3a47434
jorban43 <jordancsoehner@gmail.com>
2017-02-22 19:35:07 -0500

    first commit

---
+++ Desktop/taxCalc/001.py
@@ -0,0 +1,10 @@
+#a calculator for the a 15% tip on a meal with tax of 6.75% percent
+
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + meal * tax
+total = meal+meal*tip
+
+print("%.2f" % total)
